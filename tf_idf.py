84fa06e8d983573b734b812a4701f2fddf5ac868
Duy Tien Trinh <duytt@stud.ntnu.no>
2017-03-24 09:30:49 +0100

    Added standalone application skeleton code

--- tf_idf.py
+++ tf_idf.py
@@ -3,8 +3,21 @@ from pyspark import SparkContext, SparkConf
 from operator import add
 from pyspark.mllib.feature import HashingTF, IDF # TF-IDF specific functions 
 
-conf = SparkConf().setAppName("Data analysis with Spark")
-sc = SparkContext(conf=conf)
+
+sc = SparkContext("local", "TF-IDF: Data analysis with Spark")
+sc.setLogLevel("ERROR")
+
+# Skeleton code for standalone application
+'''
+    # need to build all dependencies and modules
+    # accepts a full path to the folder
+    # with the datasets, a flag marking whether a listing (-l) or a neighborhood
+    # (-n) should be analysed and listing id or neighborhood name on the input.
+'''
+print("TF-IDF Assignment")
+#file = sc.textFile("/home/tin/Documents/BIGData/SkeletonCodeFromJAN/application_scaffolding/python_project/data.txt").cache()
+#print("File has " + str(file.count()) + " lines.")
+#print("Passed arguments " + str(sys.argv))
 
 
 
@@ -22,4 +35,8 @@ Test built-in TF-IDF from pyspark
 '''
 
 
-rddListing = rddListing. 
+rddListing = rddListing.
+
+
+# Don't know what this does yet. Taken from skeletonCode
+sc.stop()
