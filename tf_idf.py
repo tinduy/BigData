9b274acadd2a20b87247221c1f79115b277c2834
hermanbe <hermaneidsaa@yahoo.no>
2017-03-24 09:29:47 +0100

    Added neighbourhood and description table

--- tf_idf.py
+++ tf_idf.py
@@ -6,10 +6,13 @@ from pyspark.mllib.feature import HashingTF, IDF # TF-IDF specific functions
 conf = SparkConf().setAppName("Data analysis with Spark")
 sc = SparkContext(conf=conf)
 
+rddNeighbourID = sc.textFile('/usr/local/spark/spark-2.1.0-bin-hadoop2.7/ANSWERS/6a_linkCoordinatesToNeighbourhood.csv', use_unicode = False).map(lambda x: x.split(","))
 
+fsListings = sc.textFile('/usr/local/spark/spark-2.1.0-bin-hadoop2.7/listings_us.csv', use_unicode = False)
+listingHeader = fsListings.first()
+listingsFiltered = fsListings.filter(lambda x: x!=listingHeader).map(lambda x: x.split("\t"))
 
-rddListing = sc.textFile('/home/tin/Documents/BIGData/airbnb_datasets/listings_us.csv', use_unicode = False)
-
+header2 = fsListings.first().split("\t")
 dict = {}
 for i in range(len(header2)):
     dict[header2[i]] = i
@@ -17,9 +20,13 @@ for i in range(len(header2)):
 # getIndexValue then uses the key (column name) to access right column index.
 def getIndexValue(name):
     return dict[name]
+
 '''
 Test built-in TF-IDF from pyspark
 '''
 
-
-rddListing = rddListing. 
+mappedListing = listingsFiltered.map(lambda x: (x[getIndexValue("id")], x[getIndexValue("description")]))
+joinedRDD = rddNeighbourID.join(mappedListing)
+#print(joinedRDD.take(5))
+reducedRDD = joinedRDD.map(lambda x: (x[1][0], x[1][1])).reduceByKey(add)
+print(reducedRDD.take(1))
